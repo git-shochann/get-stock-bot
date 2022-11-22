63ae3bdf6997b374636760e21c98abb864f17eb4
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-11-22 22:12:01 +0900

    refactoring

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"get-stock-bot/auth"
-	"get-stock-bot/csv"
 	"get-stock-bot/discord"
 	"get-stock-bot/stock"
-	"runtime"
-
-	"github.com/robfig/cron/v3"
 )
 
 func main() {
@@ -21,14 +17,4 @@ func main() {
 	// WebHook送信
 	discord.SendWebhook(getStockResponse)
 
-}
-
-// 定期実行する
-func init() {
-	c := cron.New()
-	setting := csv.LoadCSV()
-	monitorDelay := setting[3]
-	c.AddFunc("@every "+monitorDelay+"s", main)
-	c.Start()
-	runtime.Goexit()
 }
